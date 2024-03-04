0da409868973d97c7acf5e27ca604597c424710a
hemeng <517505440@QQ.COM>
2024-03-04 17:17:00 +0800

    6668

---
+++ src/main/java/com/example/demo/test/SonarTest.java
@@ -0,0 +1,42 @@
+package com.example.demo.test;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2024-03-04 17:14
+ */
+public class SonarTest {
+
+    public void test(String k){
+        if(k=="code") {
+
+        } else if (k=="name")  {
+
+        } else if (k=="age") {
+
+        } else if (k=="address") {
+
+        } else if (k=="phone") {
+
+        } else if (k=="email") {
+
+        } else if (k=="password") {
+
+        } else if (k=="sex") {
+
+        } else if (k=="birthday") {
+
+        } else if (k=="idCard") {
+
+        } else if (k=="bankCard") {
+
+        } else if (k=="code") {
+        }
+        )
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
